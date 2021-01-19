85ad0c7f09413e673487f8ed230c26b358401199
chunyen <panchunyen@gmail.com>
2021-01-19 17:24:00 +0800

    create function of translation & create html file

---
+++ EpubS2T.py
@@ -0,0 +1,39 @@
+import ebooklib
+import os
+from ebooklib import epub
+from opencc import OpenCC
+cc = OpenCC('s2t')
+epub_path = '/Users/chunyen/Downloads/投资最重要的事(全新升级版).epub'
+path = '/Users/chunyen/Downloads/convert'
+
+
+def mkdir(path):
+    # 判斷目錄是否存在
+    folder = os.path.exists(path)
+    if not folder:
+        # 如果不存在，則建立新目錄
+        os.makedirs(path)
+        print('-----建立成功-----')
+
+    else:
+        # 如果目錄已存在，則不建立，提示目錄已存在
+        print(path+' 目錄已存在')
+
+
+def html(path, name, text):
+    os.chdir(path)  # change address
+    create = name + '.html'  # create new html file
+    file = open(create, 'w')
+    file.write(text)  # write in content
+    file.close()
+
+
+def epubTohtml(epub_path, path):
+    mkdir(path)
+    book = epub.read_epub(epub_path)
+    for item in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+        converted = cc.convert(item.get_content())
+        html(path, item.get_name(), converted)
+
+
+epubTohtml(epub_path, path)
